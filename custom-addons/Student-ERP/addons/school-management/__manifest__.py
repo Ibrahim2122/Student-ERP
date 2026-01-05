57176fe4b566178cb9803ba7d995763375033222
Ibrahim Omar <abra56heem@gmail.com>
2026-01-05 23:08:50 +0100

    First commit

---
+++ custom-addons/Student-ERP/addons/school-management/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'School Management',
+    'version': '1.0',
+    'summary': 'School Management System',
+    'sequence': 10,
+    'description': """
+School management module to manage students, courses, and teachers.
+===========================================================""",
+    'category': 'School Management/School Management',
+    'website': 'https://ibrahimdev.cloud',
+    'author': 'Ibrahim Baatiah & Samia Busaid',
+    'depends': [],
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/student_views.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'assets': {},
+    'license': 'LGPL-3',
+}
---
+++ custom-addons/Student-ERP/addons/school-management/models/student.py
@@ -0,0 +1,36 @@
+from odoo import models, fields, api
+from datetime import date
+
+class Student(models.Model):
+    _name = 'school.student'
+    _description = 'Student'
+
+    # id = fields.Integer(string='ID', required=True)
+    full_name = fields.Char(string='Name', required=True)
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female'),
+    ], string='Gender', required=True)
+    date_of_birth = fields.Date(string='Date of Birth' , required=True)
+    email = fields.Char(string='Email', required=True)
+    phone_number = fields.Char(string='Phone Number', required=True)
+    address = fields.Text(string='Address')
+    addmission_date = fields.Date(string='Admission Date', required=True)
+    status = fields.Selection([
+        ('active', 'Active'),
+        ('inactive', 'Inactive'),
+        ('graduated', 'Graduated'),
+    ], string='Status', default='active', required=True)
+    
+    # course_ids = fields.Many2many('school.course', string='Courses')
+
+    age = fields.Integer(string='Age', compute='_compute_age', store=True)
+
+    @api.depends('date_of_birth')
+    def _compute_age(self):
+        today = date.today()
+        for rec in self:
+            if rec.date_of_birth:
+                rec.age = (today - rec.date_of_birth).days // 365
+            else:
+                rec.age = 0
